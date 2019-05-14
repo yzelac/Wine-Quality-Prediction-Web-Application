3211d2289df1646d2868b81c51a50d474c822aca
yzelac <yucheng.zhu@u.northwestern.edu>
2019-05-14 11:38:11 -0500

    mid project

---
+++ src/upload_data.py
@@ -0,0 +1,29 @@
+"""
+Created on 5/12/19
+@author: Yucheng Zhu
+"""
+
+
+import argparse
+import boto3
+s3 = boto3.client("s3")
+
+
+def upload_data(args):
+    s3.upload_file(args.input_file_path,args.bucket_name,args.output_file_path)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload data to S3")
+
+    # add argument
+    parser.add_argument("--input_file_path", help="local path for uploaded file")
+    parser.add_argument("--bucket_name", help="s3 bucket name")
+    parser.add_argument("--output_file_path", help="output path for uploaded file")
+
+    args = parser.parse_args()
+    upload_data(args)
+
+
+#e.g. python upload_data.py --input_file_path "../data/red.csv" --bucket_name "yzhu-project" --output_file_path "r2.csv"
