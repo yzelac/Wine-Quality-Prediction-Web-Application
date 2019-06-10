a9c0758448d9bf0d8b6e534e1b33e7b265a99575
yzelac <yucheng.zhu@u.northwestern.edu>
2019-06-10 17:26:00 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 3000
 APP_NAME = "wine-predictor"
 
-# local: export SQLALCHEMY_DATABASE_URI='sqlite:///data/database/churn_prediction.db'
+# local: export SQLALCHEMY_DATABASE_URI='sqlite:///data/wine_prediction.db'
 # rds: export SQLALCHEMY_DATABASE_URI="{conn_type}://{user}:{password}@{host}:{port}/{DATABASE_NAME}"
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
@@ -20,6 +20,6 @@ SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.format(conn_type=conn_type, use
 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = "0.0.0.0"
+HOST = "127.0.0.1"
 MAX_ROWS_SHOW = 30
 PATH_TO_MODEL = "models/rf_model.pkl"
